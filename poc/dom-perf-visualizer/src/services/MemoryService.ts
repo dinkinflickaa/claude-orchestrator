2f6d877a30360a9edcb4f22b62249e41a109c594
dinkinflickaa <sachin11.j@gmail.com>
2026-01-19 23:56:16 -0500

    Remove

--- poc/dom-perf-visualizer/src/services/MemoryService.ts
+++
@@ -1,9 +0,0 @@
-interface MemoryInfo {
-  usedJSHeapSize: number;
-  totalJSHeapSize: number;
-}
-
-export function getMemoryInfo(): MemoryInfo | null {
-  const perf = performance as { memory?: MemoryInfo };
-  return perf.memory ?? null;
-}
